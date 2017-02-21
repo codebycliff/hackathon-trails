b8edc680c5575f58714978f2787c3285fbf1ac60
Cliff Braton <cliff.braton@gmail.com>
2017-02-21 12:31:03 -0600

    Nest path on trails resource.

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -1,10 +1,11 @@
 class PathsController < ApplicationController
+  before_action :set_trail
   before_action :set_path, only: [:show, :edit, :update, :destroy]
 
   # GET /paths
   # GET /paths.json
   def index
-    @paths = Path.all
+    @paths = @trail.paths
   end
 
   # GET /paths/1
@@ -14,7 +15,7 @@ class PathsController < ApplicationController
 
   # GET /paths/new
   def new
-    @path = Path.new
+    @path = @trail.paths.new
   end
 
   # GET /paths/1/edit
@@ -24,11 +25,11 @@ class PathsController < ApplicationController
   # POST /paths
   # POST /paths.json
   def create
-    @path = Path.new(path_params)
+    @path = @trail.paths.new(path_params)
 
     respond_to do |format|
       if @path.save
-        format.html { redirect_to @path, notice: 'Path was successfully created.' }
+        format.html { redirect_to edit_trail_path(@trail), notice: 'Path was successfully created.' }
         format.json { render :show, status: :created, location: @path }
       else
         format.html { render :new }
@@ -42,7 +43,7 @@ class PathsController < ApplicationController
   def update
     respond_to do |format|
       if @path.update(path_params)
-        format.html { redirect_to @path, notice: 'Path was successfully updated.' }
+        format.html { redirect_to edit_trail_path(@trail), notice: 'Path was successfully updated.' }
         format.json { render :show, status: :ok, location: @path }
       else
         format.html { render :edit }
@@ -56,19 +57,23 @@ class PathsController < ApplicationController
   def destroy
     @path.destroy
     respond_to do |format|
-      format.html { redirect_to paths_url, notice: 'Path was successfully destroyed.' }
+      format.html { redirect_to edit_trail_path(@trail), notice: 'Path was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_path
-      @path = Path.find(params[:id])
-    end
+  def set_path
+    @path = Path.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def path_params
-      params.require(:path).permit(:duration, :direction, :trail_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def path_params
+    params.require(:path).permit(:duration, :direction, :trail_id)
+  end
+
+  def set_trail
+    @trail = Trail.find(params[:trail_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :paths
   root to: 'trails#index'
   resources :routes
   resources :transits
-  resources :trails
+  resources :trails do
+    resources :paths
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

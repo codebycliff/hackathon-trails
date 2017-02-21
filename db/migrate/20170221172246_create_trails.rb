585dc153ccd18927ccf19d9f07132d5214ba6853
Cliff Braton <cliff.braton@gmail.com>
2017-02-21 11:25:33 -0600

    Add trail scaffold.

---
+++ db/migrate/20170221172246_create_trails.rb
@@ -0,0 +1,10 @@
+class CreateTrails < ActiveRecord::Migration[5.0]
+  def change
+    create_table :trails do |t|
+      t.string :name
+      t.string :material
+
+      t.timestamps
+    end
+  end
+end
